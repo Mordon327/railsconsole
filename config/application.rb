f05c697fcaa697b95648c0485bb03eb682eb1c65
James Briggs <james@bzode.com>
2018-03-08 15:29:13 -0700

    Updated custom scaffold rules

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,10 @@ module RailsConsole
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+          g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :test_unit, fixture: false
+      g.stylesheets     false
+      g.javascripts     false
   end
 end
